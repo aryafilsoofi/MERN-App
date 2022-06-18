9c0dac3d59c40868e840cb7a91532636e3238994
Feelsofly <39503879+Feelsofly@users.noreply.github.com>
2022-06-18 12:10:44 -0400

    Register & Login Pages

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,7 +1,69 @@
+import { useState, useEffect } from "react";
+import { FaSignInAlt } from "react-icons/fa";
+
 function Login() {
+  const [formData, setFormData] = useState({
+    email: "",
+    password: "",
+  });
+
+  const { email, password } = formData;
+
+  const onChange = (e) => {
+    //set form data to an object
+    setFormData((prevState) => ({
+      //spread across previous state
+      ...prevState,
+      [e.target.name]: e.target.value,
+    }));
+  };
+  const onSubmit = (e) => {
+    e.preventDefault();
+  };
+
   return (
-    <div>Login</div>
-  )
+    <>
+      <section className="heading">
+        <h1>
+          <FaSignInAlt /> Login
+        </h1>
+        <p>Please Login</p>
+
+        <section className="form">
+          <form onSubmit={onSubmit}>
+            <div className="form-group"></div>
+            <div className="form-group">
+              <input
+                type="email"
+                className="form-control"
+                id="email"
+                name="email"
+                value={email}
+                placeholder="Enter your email"
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <input
+                type="password"
+                className="form-control"
+                id="password"
+                name="password"
+                value={password}
+                placeholder="Enter a password"
+                onChange={onChange}
+              />
+            </div>
+            <div className="form-group">
+              <button className="btn btn-block" type="submit">
+                Submit
+              </button>
+            </div>
+          </form>
+        </section>
+      </section>
+    </>
+  );
 }
 
-export default Login
+export default Login;
